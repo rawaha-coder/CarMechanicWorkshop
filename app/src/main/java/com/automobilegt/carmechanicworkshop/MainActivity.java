c7580136db7de4d01586e47ad9c878f12bb8ba3f
Zakariae El Kadiri <rawaha.coder@gmail.com>
2020-07-20 15:29:09 +0100

    added firebaseAuth to the App

--- app/src/main/java/com/automobilegt/carmechanicworkshop/MainActivity.java
+++ app/src/main/java/com/automobilegt/carmechanicworkshop/MainActivity.java
@@ -1,21 +1,31 @@
 package com.automobilegt.carmechanicworkshop;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String SETTING_VALUES_ID = "setting_preferences";
+    private static final String TAG = "EmailPassword";
     private boolean firstInstallation = true;
-    private Button mCarRepairButton;
-    private Button mCarDashboardButton;
-    private Button mAskMechanicButton;
 
+    //Firebase connection
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private FirebaseUser currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +44,16 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         }
+        if(currentUser == null){
+            signInAnonymously();
+        }
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        currentUser = mAuth.getCurrentUser();
     }
 
     public void carMakerList(View view) {
@@ -50,5 +69,29 @@ public class MainActivity extends AppCompatActivity {
     public void askMechanic(View view) {
         Intent intent = new Intent(getApplicationContext(), AskMechanicActivity.class);
         startActivity(intent);
+    }
+
+    private void signInAnonymously() {
+        // [START signin_anonymously]
+        mAuth.signInAnonymously()
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "Welcome to Car Mechanic Workshop App");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInAnonymously:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Connection to database failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // [START_EXCLUDE]
+                        // [END_EXCLUDE]
+                    }
+                });
+        // [END signin_anonymously]
     }
 }
--- app/src/main/java/com/automobilegt/carmechanicworkshop/util/Constants.java
+++ app/src/main/java/com/automobilegt/carmechanicworkshop/util/Constants.java
@@ -2,5 +2,5 @@ package com.automobilegt.carmechanicworkshop.util;
 
 public class Constants {
     public static String SETTING_VALUES_ID = "setting_preferences";
-    public static String MECHANIC_WORKSHOP_FOLDER ="repair/brand/";
+    public static String MECHANIC_WORKSHOP_FOLDER ="workshop/brand/";
 }
